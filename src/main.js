807064bcb930f2e07cc5fb7088e1a5f0484da4f7
Tony G <OpenToInnovate@users.noreply.github.com>
2026-08-29 14:50:31 +0100

    Our Block: cache-bust main.js to ob10

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { loadChallenges, boot } from "./council.js?v=ob9";
+import { loadChallenges, boot } from "./council.js?v=ob10";
 
 const root = document.querySelector("#desk");
 const status = document.querySelector("#boot-status");
